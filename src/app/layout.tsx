62324301f308973a475cf475c0f3719fc712d6c3
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-12-04 16:18:37 +0900

    MOA-7-test01 : git과 jira 연결 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "./globals.css"; //테스트용
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
