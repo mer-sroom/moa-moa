41c04a43c52b9298de153807711a4fbf6f60b285
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-12-18 16:35:34 +0900

    feat : test-주석제거거

--- src/app/[year]/moa/mymoa/api/route.ts
+++ src/app/[year]/moa/mymoa/api/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const headers = request.headers.get("authorization"); // 헤더 가져오기
+  const headers = request.headers.get("authorization");
   return NextResponse.json({
     message: "My Moa API Route",
     authorization: headers || "No Authorization Header",
