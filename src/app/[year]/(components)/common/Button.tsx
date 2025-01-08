a8bc0b4d0dd1c90973e19027f3b15246502a0d97
mj <ui2939@gmail.com>
2025-01-08 18:09:07 +0900

    MOA-26 Button, Sidebar Component

--- src/app/[year]/(components)/common/Button.tsx
+++ src/app/[year]/(components)/common/Button.tsx
@@ -1,4 +1,74 @@
-export default function Button({ label }: { label: string }) {
-  console.log(label)
-  return <button>{label}</button>;
+"use client";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+export interface ButtonProps {
+  label?: string;
+  className?: string;
+  size?: "small" | "medium" | "long" | "circle";
+  color?: "black" | "red";
 }
+
+//간단하게만 적었습니당
+export const buttonSize = {
+  small: css`
+    width: 50px;
+    height: 30px;
+  `,
+  medium: css`
+    width: 100px;
+    height: 30px;
+  `,
+  long: css`
+    width: 200px;
+    height: 30px;
+`,
+};
+
+export const buttonColor = {
+  black: css`
+  background-color: black;
+  color: white;
+      &:hover {
+      background-color:rgb(150, 149, 149);
+    }
+`,
+  red: css`
+  background-color: red;
+  color:black;
+      &:hover {
+      background-color:rgb(255, 123, 123);
+    }
+`,
+};
+
+const ButtonStyle = styled.button<ButtonProps>`
+  border-radius: 5px;
+  cursor: pointer;
+  white-space: nowrap;
+  border: none;
+  margin:10px;
+  overflow: hidden;
+
+  ${(props) => buttonSize[props.size]};
+  ${(props) => buttonColor[props.color]};
+`;
+
+export default function Button({
+  label,
+  size,
+  color,
+  className,
+}: ButtonProps) {
+
+  return (
+    <ButtonStyle className={className} size={size} color={color}>
+      {label}
+    </ButtonStyle>
+  )
+}
+
+
+// 사용 예제
+// <Button label="테스트" size="small" color="black"></Button>
+// <Button label="테스트" size="long" color="red"></Button>
--- src/app/[year]/(components)/common/Sidebar.tsx
+++ src/app/[year]/(components)/common/Sidebar.tsx
@@ -1,4 +1,90 @@
-export default function Sidebar() {
-  console.log("test")
-  return <div>This is a Sidebar</div>;
+"use client";
+import { useState } from "react";
+import type { CSSProperties } from "react";
+
+export interface SidebarProps {
+  userName: string;
+  width?: string;
 }
+
+export interface SidebarItem {
+  label: string;
+  href: string;
+  icon?: string;
+}
+
+const SidebarItem: SidebarItem[] = [
+  { label: "Season", href: "/season", icon: "" },
+  { label: "2025", href: "/2025", icon: "" },
+  { label: "마이페이지", href: "/mypage", icon: "" },
+];
+
+export default function Sidebar(
+  { userName, width }: SidebarProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const SidebarOpen = () => {
+    setIsOpen(true);
+  };
+
+  const SidebarClose = () => {
+    setIsOpen(false);
+  };
+
+  const SidebarStyle: CSSProperties = {
+    position: "fixed",
+    top: "0",
+    right: "0",
+    bottom: "0",
+    zIndex: "2",
+    backgroundColor: "black",
+    color: "white",
+    width: "270px",
+    height: "100%",
+    transition: "0.5s ease",
+  };
+
+  const CloseButton: CSSProperties = {
+    cursor: "pointer",
+  };
+
+  const SidebarBackground: CSSProperties = {
+    backgroundColor: "rgba(126, 126, 126, 0.7)",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    zIndex: "1",
+    width: "100%",
+    height: "100%",
+  };
+
+  return (
+    <>
+      <div
+        onClick={SidebarOpen} style={{ cursor: "pointer" }}>
+        ●●●
+      </div>
+      <div style={isOpen ? SidebarBackground : {}}></div>
+      <div style={{
+        ...SidebarStyle,
+        ...isOpen ?
+          { transform: "translateX(0)" } : { transform: "translateX(270px)" }
+      }}>
+        <ul key={userName}>
+          <h2 style={CloseButton} onClick={SidebarClose}>
+            X
+          </h2>
+          <h2>{userName}</h2>
+          {SidebarItem.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} style={{ color: "white" }}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+// 사용 예제
+// <Sidebar userName="테스트유저"></Sidebar>
