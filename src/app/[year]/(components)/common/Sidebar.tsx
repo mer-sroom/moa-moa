3c5277f867aca8ecc670364cf87f67c5d5379f13
mj <ui2939@gmail.com>
2025-01-09 15:09:41 +0900

    style: MOA-40 create Sidebar module.css / style change width props

--- src/app/[year]/(components)/common/Sidebar.tsx
+++ src/app/[year]/(components)/common/Sidebar.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useState } from "react";
-import type { CSSProperties } from "react";
+import styles from "@/styles/Sidebar.module.css";
 
 export interface SidebarProps {
   userName: string;
-  width?: string;
+  width: "mobile" | "web" | string;
 }
 
 export interface SidebarItem {
@@ -20,58 +20,34 @@ const SidebarItem: SidebarItem[] = [
 ];
 
 export default function Sidebar(
-  { userName }: SidebarProps) {
+  { userName, width }: SidebarProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [sidebarTransform, setsidebarTransform] = useState("translateX(270px)");
+  const thisWidth = width === "mobile" ? "270px" :
+    width === "web" ? "380px" :
+      "700px";
 
   const SidebarOpen = () => {
     setIsOpen(true);
-    setsidebarTransform("translateX(0)");
   };
-
   const SidebarClose = () => {
     setIsOpen(false)
-    setsidebarTransform("translateX(270px)")
-  };
-
-  const SidebarStyle: CSSProperties = {
-    position: "fixed",
-    top: "0",
-    right: "0",
-    bottom: "0",
-    zIndex: "2",
-    backgroundColor: "black",
-    color: "white",
-    width: "270px",
-    height: "100%",
-    transition: "0.5s ease",
-    transform: sidebarTransform,
-  };
-
-  const CloseButton: CSSProperties = {
-    cursor: "pointer",
-  };
-
-  const SidebarBackground: CSSProperties = {
-    backgroundColor: "rgba(126, 126, 126, 0.7)",
-    position: "fixed",
-    top: "0",
-    left: "0",
-    zIndex: "1",
-    width: "100%",
-    height: "100%",
   };
 
   return (
     <>
       <div
-        onClick={SidebarOpen} style={{ cursor: "pointer" }}>
+        onClick={SidebarOpen} className={styles.pointer}>
         ●●●
       </div>
-      <div style={isOpen ? SidebarBackground : {}}></div>
-      <div style={{ ...SidebarStyle }}>
+      <div className={isOpen ? styles.background : ""}></div>
+      <div
+        style={{
+          width: `${thisWidth}`,
+          transform: `${isOpen ? "translateX(0)" : `translateX(${thisWidth})`}`
+        }}
+        className={`${styles.sidebar}`}>
         <ul key={userName}>
-          <h2 style={CloseButton} onClick={SidebarClose}>
+          <h2 className={styles.pointer} onClick={SidebarClose}>
             X
           </h2>
           <h2>{userName}</h2>
@@ -87,4 +63,4 @@ export default function Sidebar(
 }
 
 // 사용 예제
-// <Sidebar userName="테스트유저"></Sidebar>
+// <Sidebar userName="테스트유저" width="mobile"></Sidebar>
