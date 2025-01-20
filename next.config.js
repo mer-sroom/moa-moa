394ac3c8fb1ddf072851f4a195a8044b98971924
mj <ui2939@gmail.com>
2025-01-20 10:29:52 +0900

    chore: MOA-58 next.config delete

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   webpack: (config) => {
-//     config.resolve.alias = {
-//       ...(config.resolve.alias || {}),
-//       react: require.resolve("react"),
-//     };
-//     return config;
-//   },
-// };
