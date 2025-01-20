725a4c1d55cf21773d554e0da5e205e8c5c50f61
yoon <yoooon0718@naver.com>
2025-01-20 14:15:31 +0900

    css : add css to hamburger btn icon

--- src/app/[year]/(components)/common/Navbar.tsx
+++ src/app/[year]/(components)/common/Navbar.tsx
@@ -4,21 +4,19 @@ import Image from "next/image";
 import logo from "../../../../../public/nav_logo.svg";
 import notifiIcon from "../../../../../public/nav_notification_icon.svg";
 import styles from "../../../../styles/navbar.module.css";
+
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div
-        //임시 인라인 작성, 스타일 작성법이 확정되면 css 파일로 옮길 예정
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "20px 30px",
-        }}
-      >
+      <div className={styles.navbar}>
         <Image src={logo} alt={"logo"}></Image>
         <div className={styles.btn_wrapper}>
-          <Image src={notifiIcon} alt={"notification_icon"}></Image>
+          <Image
+            src={notifiIcon}
+            alt={"notification_icon"}
+            className={styles.notification_icon}
+          ></Image>
           <div
             id={styles.hamburger_icon}
             className={isOpen ? styles.open : ""}
