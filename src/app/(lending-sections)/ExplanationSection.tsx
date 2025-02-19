bc8e4133115ef1e93fa0d14057d75bb46fe90483
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2025-02-19 09:19:41 +0900

    error fix 2

--- src/app/(lending-sections)/ExplanationSection.tsx
+++ src/app/(lending-sections)/ExplanationSection.tsx
@@ -1,34 +1,41 @@
 "use client";
 
 import React, { useState, forwardRef } from "react";
+import Image from "next/image"; // <-- Image import
 import styles from "@/styles/ExplanationSection.module.css";
 
+// "Send a letter" 섹션
 function SendLetterSection({ onClickNext }: { onClickNext: () => void }) {
   return (
     <div className={styles.subSection}>
       <h2>Send a letter</h2>
-      <p>이곳은 "Send a letter" 섹션 내용입니다.</p>
+      <p>이곳은 &quot;Send a letter&quot; 섹션 내용입니다.</p>
 
-      <img
+      <Image
         src="/assets/icons/lending/arrow-down.svg"
         alt="Next"
         className={styles.arrowDown}
+        width={40}
+        height={40}
         onClick={onClickNext}
       />
     </div>
   );
 }
 
+// "Get a letter" 섹션
 function GetLetterSection({ onClickNext }: { onClickNext: () => void }) {
   return (
     <div className={styles.subSection}>
       <h2>Get a letter</h2>
-      <p>이곳은 "Get a letter" 섹션 내용입니다.</p>
+      <p>이곳은 &quot;Get a letter&quot; 섹션 내용입니다.</p>
 
-      <img
+      <Image
         src="/assets/icons/lending/arrow-down.svg"
         alt="Next"
         className={styles.arrowDown}
+        width={40}
+        height={40}
         onClick={onClickNext}
       />
     </div>
