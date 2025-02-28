217ffafd539c9191cb9ffd2b0b00333606296450
yoon <yoooon0718@naver.com>
2025-02-28 11:52:13 +0900

    feat: replace insta with line share / edit UI for link copying

--- src/app/[year]/moa/mymoa/(components)/ShareLinkContent.tsx
+++ src/app/[year]/moa/mymoa/(components)/ShareLinkContent.tsx
@@ -1,102 +1,132 @@
 "use client";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import { useParams } from "next/navigation";
 import copyIcon from "@/../../public/assets/icons/sharelink_modal/copy_link.svg";
 import kakaoIcon from "@/../../public/assets/icons/kakao.svg";
-import instaIcon from "@/../../public/assets/icons/sharelink_modal/insta.svg";
+import lineIcon from "@/../../public/assets/icons/sharelink_modal/line.svg";
 import facebookIcon from "@/../../public/assets/icons/sharelink_modal/facebook.svg";
 import xIcon from "@/../../public/assets/icons/sharelink_modal/x_sns.svg";
-import { useParams } from "next/navigation";
+import Swal from "sweetalert2";
+import styles from "@/styles/modal.module.css";
 
-const snsItems = [
+interface SnsItem {
+  id: string;
+  label: string;
+  shareHref: string;
+  icon: StaticImageData;
+}
+
+const snsItems: SnsItem[] = [
   {
     id: "1",
     label: "카카오",
-    href: "/2025/moa/select-moa",
+    shareHref: "https://story.kakao.com/share?url=",
     icon: kakaoIcon,
   },
   {
     id: "2",
-    label: "인스타",
-    href: "",
-    icon: instaIcon,
+    label: "엑스",
+    shareHref: "https://twitter.com/intent/tweet?url=",
+    icon: xIcon,
   },
   {
     id: "3",
     label: "페이스북",
-    href: "/2025/sent-letter",
+    shareHref: "http://www.facebook.com/sharer/sharer.php?u=",
     icon: facebookIcon,
   },
   {
     id: "4",
-    label: "엑스",
-    href: "/2025/friendlist",
-    icon: xIcon,
+    label: "라인",
+    shareHref: "https://line.me/R/msg/text/?",
+    icon: lineIcon,
   },
 ];
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
 export default function ShareLinkModal() {
   const { id } = useParams();
+  const currentUrl = `${baseUrl}/2025/moa/mymoa/${id}`;
+
+  // 클립보드 복사 함수 (비동기)
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(currentUrl).catch(e => {
+      console.error(e);
+      Swal.fire({
+        title: "Error",
+        text: "복사 중 문제가 생겼습니다",
+        icon: "error",
+        confirmButtonText: "확인",
+      });
+    });
+  };
+
+  // SNS 공유하기 & 클립보드 복사
+  const handleShare = snsUrl => {
+    const url = snsUrl + encodeURIComponent(currentUrl);
+    window.open(url, "_blank");
+    // 혹시 몰라 클립보드 복사 실행
+    copyToClipboard();
+  };
+
+  // 링크 복사 버튼 이벤트
+  const handleCopyLink = async () => {
+    try {
+      //HTTPS 경우
+      if (window.location.protocol === "https:") {
+        await copyToClipboard();
+      }
+      //HTTP 경우
+      else {
+        const textArea = document.createElement("textarea");
+        textArea.value = currentUrl;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textArea);
+      }
+      Swal.fire({
+        toast: true,
+        position: "bottom",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        icon: "success",
+        title: "링크 복사 완료!",
+        didOpen: toast => {
+          toast.addEventListener("mouseenter", Swal.stopTimer);
+          toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+      });
+    } catch (e) {
+      console.error(e);
+      Swal.fire({
+        title: "Error",
+        text: "복사 중 문제가 생겼습니다",
+        icon: "error",
+        confirmButtonText: "확인",
+      });
+    }
+  };
   return (
-    <div
-      style={{
-        paddingTop: "32px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          width: "90%",
-          justifyContent: "space-between",
-          alignItems: "center",
-          gap: "20px",
-        }}
-      >
-        <input
-          value={`${baseUrl}/2025/moa/mymoa/${id}`}
-          readOnly
-          style={{
-            backgroundColor: "var(--color-gray-200)",
-            border: "none",
-            borderRadius: "52px",
-            padding: "15px 15px",
-            fontSize: "16px",
-            width: "100%",
-            maxWidth: "282px",
-          }}
-        />
-        <Image src={copyIcon} alt="link copy icon" onClick={() => {}} />
+    <div className={styles.contentWrapper}>
+      <div className={styles.linkCopyWrapper}>
+        <input value={currentUrl} readOnly className={styles.linkInput} />
+        <button className={styles.linkCopyBtn}>
+          <Image src={copyIcon} alt="link copy icon" onClick={handleCopyLink} />
+        </button>
       </div>
       <div>
-        <ul
-          style={{
-            margin: "0",
-            display: "flex",
-            padding: "28px 0 8px 0",
-            justifyContent: "space-between",
-            width: "100%",
-            maxWidth: "282px",
-            gap: "16px",
-            alignItems: "center",
-            alignSelf: "stretch",
-          }}
-        >
+        <ul className={styles.snsListWrapper}>
           {snsItems.map(item => (
-            <li
-              key={item.id}
-              style={{
-                backgroundColor: "var(--color-black)",
-                borderRadius: "50%",
-                width: "55px",
-                height: "55px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Image src={item.icon} alt={item.label} onClick={() => {}} />
+            <li key={item.id}>
+              <button
+                type="button"
+                onClick={() => handleShare(item.shareHref)}
+                className={styles.snsItem}
+              >
+                <Image src={item.icon} alt={item.label} />
+              </button>
             </li>
           ))}
         </ul>
