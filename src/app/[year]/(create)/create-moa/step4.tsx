9d547c732ab54030d5231ff9129848217b352c60
mj <ui2939@gmail.com>
2025-05-06 14:57:42 +0900

    feat: create moa step4 - pop up (MOA-106)

--- src/app/[year]/(create)/create-moa/step4.tsx
+++ src/app/[year]/(create)/create-moa/step4.tsx
@@ -2,12 +2,33 @@
 import styles from "@/styles/createMoa.module.css";
 import Button from "../../(components)/common/Button";
 import ToggleButton from "../(components)/ToggleButton";
+import { IoCloseOutline  } from "react-icons/io5";
+import { useState } from 'react';
 
 
 export default function CreateMoaStep4() {
+  const [visible, setVisible] = useState(true);
 
+  const handleClose = () => {
+    setVisible(false);
+  };
 
-  return <>
+  return <div className={styles.step4_Container}>
+    <div>
+      {visible && (
+        <div className={styles.popup}>
+          <button className={styles.popup_closeButton}
+            onClick={handleClose}>
+            <IoCloseOutline  color="white" fontSize={"25px"}/>
+          </button>
+          <div className={styles.popup_font}>
+            <p>버튼을 검은색으로 만들어</p>
+            <p>기능을 활성화 할 수 있어요</p>
+          </div>
+        </div>
+      )}
+
+    </div>
     <h1>모아 설정</h1>
     <p className={styles.line_sort_gray}>한 번 설정한 값은 수정할 수 없습니다</p>
     <div className={styles.toggle_group}>
@@ -15,15 +36,15 @@ export default function CreateMoaStep4() {
         <span>비로그인 유저에게도 편지 받기</span>
         <div className={styles.toggle_right}>
           <ToggleButton label="비로그인 유저에게도 편지 받기"></ToggleButton>
-          </div>
+        </div>
       </div>
       <div>
         <span>받은 편지 개수 공개하기</span>
         <div className={styles.toggle_right}>
           <ToggleButton label="받은 편지 개수 공개하기"></ToggleButton>
-          </div>
+        </div>
       </div>
     </div>
     <Button label="생성하기" size="next" color="black"></Button>
-  </>
+  </div>
 }
