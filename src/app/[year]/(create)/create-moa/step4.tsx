063ca8eabee1f73cb2308f57b4db4d22e4623de4
mj <ui2939@gmail.com>
2025-05-08 14:45:26 +0900

    feat: create moa step4 - div tag line-up (MOA-104)

--- src/app/[year]/(create)/create-moa/step4.tsx
+++ src/app/[year]/(create)/create-moa/step4.tsx
@@ -28,19 +28,21 @@ export default function CreateMoaStep4() {
       )}
 
     </div>
-    <h1>모아 설정</h1>
-    <p className={styles.line_sort_gray}>한 번 설정한 값은 수정할 수 없습니다</p>
-    <div className={styles.toggle_group}>
+    <div className={styles.step4_setting_group}>
+      <h1>모아 설정</h1>
+      <p className={styles.line_sort_gray}>한 번 설정한 값은 수정할 수 없습니다</p>
       <div>
-        <span>비로그인 유저에게도 편지 받기</span>
-        <div className={styles.toggle_right}>
-          <ToggleButton label="비로그인 유저에게도 편지 받기"></ToggleButton>
+        <div className={styles.span_group}>
+          <span>비로그인 유저에게도 편지 받기</span>
+          <div className={styles.toggle_right}>
+            <ToggleButton label="비로그인 유저에게도 편지 받기"></ToggleButton>
+          </div>
         </div>
-      </div>
-      <div>
-        <span>받은 편지 개수 공개하기</span>
-        <div className={styles.toggle_right}>
-          <ToggleButton label="받은 편지 개수 공개하기"></ToggleButton>
+        <div className={styles.span_group}>
+          <span>받은 편지 개수 공개하기</span>
+          <div className={styles.toggle_right}>
+            <ToggleButton label="받은 편지 개수 공개하기"></ToggleButton>
+          </div>
         </div>
       </div>
     </div>
