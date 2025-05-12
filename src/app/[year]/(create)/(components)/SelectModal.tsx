4b3f644688434b3dd284ad9d1388761a2878ee21
mj <ui2939@gmail.com>
2025-05-12 16:26:33 +0900

    feat: create moa step 3 - member list color white/black (MOA-104)

--- src/app/[year]/(create)/(components)/SelectModal.tsx
+++ src/app/[year]/(create)/(components)/SelectModal.tsx
@@ -1,20 +1,21 @@
 import styles from "@/styles/SelectModal.module.css";
 import Button from "../../(components)/common/Button";
 import { FiPlusCircle } from "react-icons/fi";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ check, state }: { check: () => void, state: boolean }) => {
     return (
         <div>
-            <div className={styles.list_container}>
+            <div className={`${styles.list_container} ${state ? styles.check : ''}`} >
                 <div className={styles.list_circle}>
-
                 </div>
-                <div className={styles.list_text_box}>
+                <div className={`${styles.list_text_box} ${state ? styles.check : ''}`} >
                     <p>머가문</p>
                     <p>초대가능</p>
                 </div>
-                <div className={styles.list_icon}>
-                <FiPlusCircle color="gray" fontSize="30px"/>
+                <div className={styles.list_icon} onClick={check}>
+                    {state ? <FaRegCircleCheck color="white" fontSize="30px" /> : <FiPlusCircle color="gray" fontSize="30px" />}
                 </div>
             </div>
         </div>
@@ -22,6 +23,9 @@ const List = () => {
 }
 
 export default function SelectModal({ onClose }: { onClose: () => void }) {
+    const [ischeck, setisCheck] = useState(false);
+    const check = () => setisCheck(!ischeck);
+
     return (
         <div className={styles.selectModal_container}>
             <div className={styles.selectModal_main}>
@@ -31,7 +35,7 @@ export default function SelectModal({ onClose }: { onClose: () => void }) {
                         className={styles.input} />
                     <div className={styles.middle}>
                         <p>초대 가능한 친구</p>
-                        <List></List>
+                        <List check={check} state={ischeck}></List>
                     </div>
                     <div className={styles.bottom_tag}>
                         <p className={styles.selectModal_close} onClick={onClose}>
@@ -40,9 +44,7 @@ export default function SelectModal({ onClose }: { onClose: () => void }) {
                         <p className={styles.selectModal_select}>
                             선택 완료
                         </p>
-
                     </div>
-
                     <div className={styles.selectModal_select_button}>
                         <Button label="선택완료" size="modalBtn" color="black"></Button>
                     </div>
