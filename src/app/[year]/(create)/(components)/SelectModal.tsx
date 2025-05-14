92d69e831fc596896c9454091758e059c8b3697e
mj <ui2939@gmail.com>
2025-05-14 23:34:02 +0900

    feat: create moa step 3 - member list data change to <string[]>

--- src/app/[year]/(create)/(components)/SelectModal.tsx
+++ src/app/[year]/(create)/(components)/SelectModal.tsx
@@ -2,14 +2,14 @@ import styles from "@/styles/SelectModal.module.css";
 import Button from "../../(components)/common/Button";
 import { FiPlusCircle } from "react-icons/fi";
 import { FaRegCircleCheck } from "react-icons/fa6";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 
 const List = ({
-    check, onDateSend, state, name
+    check, state, name
 }: {
-    check: () => void,
-    onDateSend: (data: string) => void,
+    check: (name: string) => void,
     state: boolean,
     name: string
 }) => {
@@ -22,7 +22,7 @@ const List = ({
                     <p className={styles.bold}>{name}</p>
                     <p>초대가능</p>
                 </div>
-                <div className={styles.list_icon} onClick={() => { check(); onDateSend(name); }}>
+                <div className={styles.list_icon} onClick={() => { check(name); }}>
                     {state ?
                         <FaRegCircleCheck color="white" fontSize="30px" /> :
                         <FiPlusCircle color="gray" fontSize="30px" />}
@@ -32,34 +32,88 @@ const List = ({
     )
 }
 
+type MemberType = {
+    name: string;
+    selected: boolean;
+};
+
 export default function SelectModal({
     onClose, onMemberChange
 }: {
     onClose: () => void,
-    onMemberChange: (data: string) => void
+    onMemberChange: (data: string[]) => void
 }) {
+
     const [ischeck, setisCheck] = useState(false);
-    const check = () => setisCheck(!ischeck);
+    const [member, setMember] = useState<string[]>([]);
 
     //임시데이터
-    let name = "머가문"
-    const [member, setMember] = useState<string>('');
+    const [friend, setFriend] = useState<MemberType[]>([
+        { name: "머가문", selected: false },
+        { name: "멈가문", selected: false },
+        { name: "현가문", selected: false },
+    ]);
 
-    const onDateChange = (data: string) => {
-        setMember(data)
-        console.log("setMember 확인")
+    useEffect(() => {
+        const booleanCheck = friend.filter((friend) => friend.selected).length;
+        if (booleanCheck === 0) {
+            setisCheck(false);
+        } else {
+            setisCheck(true);
+        }
+    }, [friend]);
+
+    useEffect(() => {
+        const onDateChange = friend.filter((friend) => friend.selected)
+            .map((friend) => friend.name);
+        setMember(onDateChange);
+    }
+        , [friend]);
+
+    // ["머가문", "멈가문", "현가문"] 의 boolean 값 상태체크
+    const check = (name_: string) =>
+        setFriend((prev) =>
+            prev.map((friend) =>
+                friend.name === name_
+                    ? { ...friend, selected: !friend.selected }
+                    : friend
+            )
+        );
+
+    //친구선택 목록에서 x버튼으로 이름 빼기
+    const removeMember = (remove: string) => {
+        setMember(prevMember => prevMember.filter((memberName, idx) => memberName !== remove));
     };
 
     return (
         <div className={styles.selectModal_container}>
             <div className={styles.selectModal_main}>
                 <div className={styles.selectModal_box}>
-                    <input type="text"
-                        placeholder="선택한 친구 목록"
-                        className={styles.input} value={member} />
+                    <div className={styles.input}>
+                        {ischeck ?
+                            <>{member.map((member, id) => (
+                                <p key={id}>
+                                    {member}
+                                    <IoMdClose
+                                        className={styles.close_icon}
+                                        size="20px"
+                                        onClick={() => removeMember(member)} />
+                                </p>
+                            ))}</>
+                            :
+                            <p className={styles.placeholder}>선택한 친구 목록</p>}
+                    </div>
                     <div className={styles.middle}>
                         <p>초대 가능한 친구</p>
-                        <List check={check} state={ischeck} name={name} onDateSend={onDateChange}></List>
+                        {friend.map((name) => (
+                            <div key={name.name} className={styles.middle_list}>
+                                <List
+                                    check={check}
+                                    state={name.selected}
+                                    name={name.name}>
+                                </List>
+                            </div>
+                        ))}
                     </div>
                     <div className={styles.bottom_tag}>
                         <p className={styles.selectModal_close} onClick={onClose}>
@@ -72,7 +126,7 @@ export default function SelectModal({
                                 <Button label="선택완료" size="modalBtn" color="black"></Button>
                             </div> :
                             <div className={styles.notPointer}>
-                            <Button label="선택완료" size="modalBtn" color="none"></Button>
+                                <Button label="선택완료" size="modalBtn" color="none"></Button>
                             </div>
                         }
                     </div>
