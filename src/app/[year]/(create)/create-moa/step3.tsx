7f4942f45722dac0207dcf045f36d80553cb3fe5
mj <ui2939@gmail.com>
2025-06-02 16:19:25 +0900

    feat: create moa step 3 - group member input border color event css

--- src/app/[year]/(create)/create-moa/step3.tsx
+++ src/app/[year]/(create)/create-moa/step3.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Calendar from "../(components)/Calendar";
 import dayjs from 'dayjs';
 import Button from "../../(components)/common/Button";
@@ -20,7 +20,9 @@ export default function CreateMoaStep3<NextStepProps>({ nextStep }) {
   const date = new Date().getDate();
   const [isOpen, setIsOpen] = useState(false);
   const [color, setColor] = useState(false);
+  const [memberColor, setmemberColor] = useState(false);
   const [openSelect, setOpenSelect] = useState(false);
+  const [member, setMember] = useState<string[]>([]);
 
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -52,9 +54,12 @@ export default function CreateMoaStep3<NextStepProps>({ nextStep }) {
     console.log("입력완")
     setColor(!!e.currentTarget.value)
   };
+  const oninputColor_member = (e: React.FocusEvent<HTMLInputElement>) => {
+    console.log("입력완")
+    setmemberColor(!!member)
+  };
 
   //select modal.tsx에서 받아온 이름 적용
-  const [member, setMember] = useState<string[]>([]);
   const onDateChange = (data: string[]) => {
     setMember(data)
     console.log(`${"setMember 확인 : " + member}`)
@@ -115,9 +120,10 @@ export default function CreateMoaStep3<NextStepProps>({ nextStep }) {
               <input id="group_member"
                 disabled={!isOpen}
                 placeholder="함께 할 친구를 설정해 주세요"
-                className={styles.group_member_input}
+                className={`${styles.group_member_input} ${memberColor ? styles.color : ''}`}
                 onClick={() => setOpenSelect(true)}
                 value={member}
+                onBlur={oninputColor_member}
               />
             </div>
           </form>
