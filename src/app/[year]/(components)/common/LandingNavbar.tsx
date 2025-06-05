3d86e2eb6e13e1db7a33ffbc590a997e0f3dc58b
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2025-06-05 09:57:25 +0900

    navbar-fix

--- src/app/[year]/(components)/common/LandingNavbar.tsx
+++ src/app/[year]/(components)/common/LandingNavbar.tsx
@@ -2,39 +2,45 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useSession } from "next-auth/react"; // 추가함*
+import { useSession } from "next-auth/react";
+import { format } from "date-fns";
 import logo from "../../../../../public/assets/icons/nav_sidebar/nav_logo.svg";
 import styles from "../../../../styles/LandingNavbar.module.css";
 
 export default function LandingNavbar() {
-  const { data: session } = useSession(); // 추가함*
-  const isLoggedIn = !!session?.user; // 추가함*
+  const { data: session } = useSession();
+  const isLoggedIn = !!session?.user;
+
+  const currentYear = format(new Date(), "yyyy");
 
   return (
     <header className={styles.navbar}>
-      {/* 로고 영역 */}
-      <div>
-        <Link href="/">
-          <Image
-            src={logo}
-            alt="moa-logo"
-            className={styles.logo_icon}
-            width={54}
-            height={20}
-          />
-        </Link>
-      </div>
+      {/* 로고 */}
+      <Link href="/">
+        <Image
+          src={logo}
+          alt="moa-logo"
+          className={styles.logo_icon}
+          width={54}
+          height={20}
+        />
+      </Link>
 
-      {/* 메뉴(버튼) 영역 */}
+      {/* 메뉴 */}
       <nav className={styles.btn_wrapper}>
-        <Link href="/moa/select-moa" className={styles.menuBtn}>
+        {/* start ➜ /2025/moa/select-moa (올해 자동) */}
+        <Link
+          href={`/${currentYear}/moa/select-moa`}
+          className={styles.menuBtn}
+        >
           start
         </Link>
+
         <Link href="/about" className={styles.menuBtn}>
           About
         </Link>
 
-        {isLoggedIn /* 추가함* */ ? (
+        {isLoggedIn ? (
           <span className={styles.menuBtn}>
             {`환영합니다 ${session.user.name ?? session.user.email} 님`}
           </span>
