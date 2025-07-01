d12f0581879815bc22e3c5d20bfdf94599d1249c
mj <ui2939@gmail.com>
2025-07-01 15:04:25 +0900

    chore: delete unused hooks

--- src/app/(lending-sections)/HomeSection.tsx
+++ src/app/(lending-sections)/HomeSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import styles from "@/styles/HomeSection.module.css";
 import Hlighter from "./hlighter";
